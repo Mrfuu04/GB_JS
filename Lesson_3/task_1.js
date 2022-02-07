13c246d97b628d7d22bfb30d2b871092d60569a6
mrfuu04 <mrfuu04@gmail.com>
2022-02-07 14:21:06 +0300

    correct task_1

--- Lesson_3/task_1.js
+++ Lesson_3/task_1.js
@@ -1,6 +1,15 @@
 // С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 
-let i = 0;
-while (i <= 100) {
-    console.log(i++);
+let i = 3;
+
+console.log(2);
+while (i < 100) {
+    let j = 2;
+    let cnt = 0;
+    while (i > j) {
+        if (i % j == 0) cnt++;
+        j++;
+    }
+    if (cnt == 0) console.log(i);
+    i++;
 }
